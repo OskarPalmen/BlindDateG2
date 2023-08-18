ee2ddd356b2fbdf22dbd47b0f7e774ff69d9d3fa
AxelAEDU <axel.almeren@edu.futuregames.nu>
2023-08-18 14:23:12 +0200

    update with new

--- Assets/Scrips/LobbyUI/TestingGameManager.cs
+++ Assets/Scrips/LobbyUI/TestingGameManager.cs
@@ -4,34 +4,59 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestingGameManager : NetworkBehaviour
 {
+    [SerializeField] private Transform playerPrefab;
     public static TestingGameManager Instance { get; private set; }
 
-    public event EventHandler OnLocalPlayerReadyChanged;
+    //public event EventHandler OnLocalPlayerReadyChanged;
 
-    private bool isLocalPlayerReady;
-    private Dictionary<ulong, bool> playerReadyDictionary;
-
-
+    //private bool isLocalPlayerReady;
+    //private Dictionary<ulong, bool> playerReadyDictionary;
 
 
-    private void Awake()
+
+
+    //private void Awake()
+    //{
+    //    playerReadyDictionary = new Dictionary<ulong, bool>();
+    //}
+
+
+    //private void GameInput_OnInteractAction(object sender, EventArgs e)
+    //{
+    //    isLocalPlayerReady = true;
+    //    OnLocalPlayerReadyChanged?.Invoke(this, EventArgs.Empty);
+    //}
+
+
+    //public bool IsLocalPlayerReady()
+    //{
+    //    return isLocalPlayerReady;
+    //}
+
+
+    public override void OnNetworkSpawn()
     {
-        playerReadyDictionary = new Dictionary<ulong, bool>();
+        if (IsServer)
+        {
+            NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += SceneManager_OnLoadEventComplete;
+        }
     }
 
-
-    private void GameInput_OnInteractAction(object sender, EventArgs e)
+    private void SceneManager_OnLoadEventComplete(string sceneName, LoadSceneMode loadSceneMode, List<ulong> clientsCompleted, List<ulong> clientsTimedOut)
     {
-        isLocalPlayerReady = true;
-        OnLocalPlayerReadyChanged?.Invoke(this, EventArgs.Empty);
-    }
-
-
-    public bool IsLocalPlayerReady()
-    {
-        return isLocalPlayerReady;
+        if (NetworkManager.Singleton.ConnectedClientsIds.Count >= 2)
+        {
+            foreach (ulong clinetId in NetworkManager.Singleton.ConnectedClientsIds)
+            {
+                //spawning in the player prefab
+                Transform playerTransform = Instantiate(playerPrefab);
+                playerTransform.GetComponent<NetworkObject>().SpawnAsPlayerObject(clinetId, true);
+            }
+        }
+        
     }
 }
