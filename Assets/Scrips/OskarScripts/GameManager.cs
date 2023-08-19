829f053690e853d35c32ed3d7f3cc150ffef4bda
Oskar <oskarpalmen00@gmail.com>
2023-08-19 15:33:37 +0200

    Im done

--- Assets/Scrips/OskarScripts/GameManager.cs
+++ Assets/Scrips/OskarScripts/GameManager.cs
@@ -5,6 +5,10 @@ public class GameManager : MonoBehaviour
 {
     public GameObject guessButton;
 
+    private ClickableImageToggle untoggledCharacter; // Reference to the untoggled character
+    public GameObject correctPopUp;
+    public GameObject wrongPopUp;
+
     private void Start()
     {
         // Find all the ClickableImageToggle components in the scene
@@ -26,10 +30,46 @@ public class GameManager : MonoBehaviour
             if (!toggle.IsChecked)
             {
                 untoggledCharacterCount++;
+                untoggledCharacter = toggle; // Store the untoggled character reference
             }
         }
 
         // Show the guess button when there's only one untoggled character
         guessButton.SetActive(untoggledCharacterCount == 1);
+    }
+
+    public void MakeGuess()
+    {
+        if (untoggledCharacter != null)
+        {
+            string untoggledCharacterTag = untoggledCharacter.gameObject.tag;
+            Debug.Log("Untoggled Character Tag: " + untoggledCharacterTag);
+
+            if (untoggledCharacterTag == "PlayerCharacter")
+            {
+                Debug.Log("You guessed the player character! You Win");
+
+                correctPopUp.SetActive(true);
+                wrongPopUp.SetActive(false);
+                correctPopUp.transform.SetAsLastSibling();
+                wrongPopUp.transform.SetAsLastSibling();
+            }
+            else if (untoggledCharacterTag == "NPCCharacter")
+            {
+                Debug.Log("You guessed an NPC character! You lose LOL");
+                correctPopUp.SetActive(false);
+                wrongPopUp.SetActive(true);
+                correctPopUp.transform.SetAsLastSibling();
+                wrongPopUp.transform.SetAsLastSibling();
+            }
+            else
+            {
+                Debug.Log("Unknown character tag: " + untoggledCharacterTag);
+            }
+        }
+        else
+        {
+            Debug.Log("No untoggled character found.");
+        }
     }
 }
